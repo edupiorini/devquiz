b7c144e1af22bedcd9db4b4214b3093afffec877
Eduardo Piorini <eduardopiorini@hotmail.com>
2021-04-22 07:43:15 -0300

    reading dinamicly (kinda)

--- lib/home/widgets/appbar/app_bar_widget.dart
+++ lib/home/widgets/appbar/app_bar_widget.dart
@@ -1,9 +1,11 @@
 import 'package:DevQuiz/core/core.dart';
 import 'package:DevQuiz/home/widgets/scorecard/score_card_widget.dart';
+import 'package:DevQuiz/shared/widgets/progress_indicator/models/user_model.dart';
 import 'package:flutter/material.dart';
 
 class AppBarWidget extends PreferredSize {
-  AppBarWidget()
+  final UserModel user;
+  AppBarWidget({required this.user})
       : super(
             preferredSize: Size.fromHeight(250),
             child: Container(
@@ -24,7 +26,7 @@ class AppBarWidget extends PreferredSize {
                               style: AppTextStyles.title,
                               children: [
                                 TextSpan(
-                                  text: "Laura Piorini",
+                                  text: user.name,
                                   style: AppTextStyles.titleBold,
                                 )
                               ],
@@ -36,8 +38,7 @@ class AppBarWidget extends PreferredSize {
                             decoration: BoxDecoration(
                               borderRadius: BorderRadius.circular(10),
                               image: DecorationImage(
-                                image: NetworkImage(
-                                    'https://scontent.fsjk2-1.fna.fbcdn.net/v/t1.6435-9/119169942_10224345399406366_8697816959624030214_n.jpg?_nc_cat=102&ccb=1-3&_nc_sid=09cbfe&_nc_eui2=AeE-38c41lZkXMfz03jR4kdmA3mN9SVm-14DeY31JWb7Xui-ub79megI-IxsqCWZl9I&_nc_ohc=QszmX8iEolcAX8N8OKM&_nc_ht=scontent.fsjk2-1.fna&oh=d5b48644d0fdf189ee36192dbba0a6df&oe=60A3D4EE'),
+                                image: NetworkImage(user.photoUrl),
                               ),
                             ),
                           )
